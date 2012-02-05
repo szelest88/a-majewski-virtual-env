4f2e9dc32467e0d376beb12b03a24c349a7921a6
szelest88@gmail.com <szelest88@gmail.com@e6cc42a2-7d04-1938-8898-ab62938aa935>
2012-02-05 06:36:14 +0000

    Ok... Wciąż działa.

--- Hanoi/src/edu/dhbw/andar/sample/HttpHelper.java
+++ Hanoi/src/edu/dhbw/andar/sample/HttpHelper.java
@@ -4,14 +4,25 @@ import java.io.*;
 
 
 public class HttpHelper {
-public static int getContent(){
+	URL urlGet;
+	URL urlSet;
+
+	public HttpHelper(String urlString){ //http://192.168.1.2/server/
+		try {
+			urlGet = new URL(urlString+"data.txt");
+			urlSet = new URL(urlString+"set.php?data=-1");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+public int getContent(){
 //192.168.1.2
 int res=-1;
-	URL url;
 	try {
-		url = new URL("http://192.168.1.2/server/data.txt");
+		//urlGet = new URL("http://192.168.1.2/server/data.txt");
 	
-    URLConnection conn = url.openConnection();
+    URLConnection conn = urlGet.openConnection();
     // Get the response
     BufferedReader rd = new BufferedReader(new 
 InputStreamReader(conn.getInputStream()));
@@ -35,20 +46,18 @@ InputStreamReader(conn.getInputStream()));
 	
 	return res;
 }
-public static void setNull(){
+public void setNull(){
 	//192.168.1.2
-	int res=-1;
-		URL url;
+
 		try {
-			url = new URL("http://192.168.1.2/server/set.php?data=-1");
+			//urlSet = new URL("http://192.168.1.2/server/set.php?data=-1");
 		//"http://localhost/server/set.php?data=" + current
-	    URLConnection conn = url.openConnection();
+	    URLConnection conn = urlSet.openConnection();
 	    // Get the response
 	    BufferedReader rd = new BufferedReader(new 
 	InputStreamReader(conn.getInputStream()));
 	    String line = "";
 	    while ((line = rd.readLine()) != null) {
-		res = Integer.parseInt(line);
 	    }
 	    
 	    
@@ -64,6 +73,5 @@ public static void setNull(){
 		}
 		
 		
-	//	return res;
 	}
 }
